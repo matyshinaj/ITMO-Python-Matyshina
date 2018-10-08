3f8df69df5dfeb2dea394e6d65d36fb1552f717a
matyshinaj <genya2001.6@mail.ru>
2018-10-08 12:24:49 +0300

    Add vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,28 +1,26 @@
+L = ('A','B', 'C', 'D', 'E', 'F', 'G','H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+l = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
 def encrypt_vigenere(plaintext, keyword):
-    """
-    Encrypts plaintext using a Vigenere cipher.
-
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
-    # PUT YOUR CODE HERE
+    ciphertext = ''
+    keyword *= (len(plaintext)//len(keyword)) + 1
+    for index, element in enumerate(plaintext):
+        if element in (L):
+            ciphertext += chr((ord(element)+ord(keyword[index])) % 26 + ord('A'))
+        elif element in (l):
+            ciphertext += chr((ord(element) + ord(keyword[index])) % 26 + ord('a'))
+        else:
+            ciphertext += element
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext, keyword):
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
-
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
-    # PUT YOUR CODE HERE
+    plaintext = ''
+    keyword *= (len(ciphertext) // len(keyword)) + 1
+    for index, element in enumerate(ciphertext):
+        if element in (L):
+            plaintext += chr((ord(element) - ord(keyword[index])) % 26 + ord('A'))
+        elif element in (l):
+            plaintext += chr((ord(element) - ord(keyword[index])) % 26 + ord('a'))
+        else:
+            plaintext += element
     return plaintext
