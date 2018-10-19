812e3117fb35f5b28176a0d0b94dabe3280b415c
matyshinaj <43173617+matyshinaj@users.noreply.github.com>
2018-10-19 21:00:44 +0300

    добавлены element.isupper(),element.islower()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,10 +1,8 @@
-L = ('A','B', 'C', 'D', 'E', 'F', 'G','H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
-l = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
 def encrypt_vigenere(plaintext:str, keyword:str) -> str:
     ciphertext = ''
     keyword *= (len(plaintext)//len(keyword)) + 1
     for index, element in enumerate(plaintext):
-        if element in (L):
+        if element.isupper():
             ciphertext += chr((ord(element)+ord(keyword[index])) % 26 + ord('A'))
         elif element in (l):
             ciphertext += chr((ord(element) + ord(keyword[index])) % 26 + ord('a'))
@@ -17,7 +15,7 @@ def decrypt_vigenere(ciphertext:str, keyword:str) -> str:
     plaintext = ''
     keyword *= (len(ciphertext) // len(keyword)) + 1
     for index, element in enumerate(ciphertext):
-        if element in (L):
+        elif element.islower():
             plaintext += chr((ord(element) - ord(keyword[index])) % 26 + ord('A'))
         elif element in (l):
             plaintext += chr((ord(element) - ord(keyword[index])) % 26 + ord('a'))
